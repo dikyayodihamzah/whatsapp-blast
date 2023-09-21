7ace96e519cf02a80ab039c9794e0134a42b4a26
dikyayodihamzah <dikyayodihamzah@gmail.com>
2023-09-21 14:39:40 +0700

    completed main function

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/dikyayodihamzah/whatsapp-blast
+
+go 1.21.1
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/maskentir/qontalk v1.2.6
+)
--- main.go
+++ main.go
@@ -1,1 +1,50 @@
 package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/maskentir/qontalk/qontak"
+	_ "github.com/joho/godotenv/autoload"
+)
+
+func main() {
+
+	var (
+		username     = os.Getenv("QONTAK_USERNAME")
+		password     = os.Getenv("QONTAK_PASSWORD")
+		grantType    = os.Getenv("QONTAK_GRANT_TYPE")
+		clientID     = os.Getenv("QONTAK_CLIENT_ID")
+		clientSecret = os.Getenv("QONTAK_CLIENT_SECRET")
+
+		messageTemplateID = os.Getenv("QONTAK_MESSAGE_TEMPLATE_ID")
+		channelIntegrationID = os.Getenv("QONTAK_CHANNEL_INTEGRATION_ID")
+	)
+
+	sdk := qontak.NewQontakSDKBuilder().WithClientCredentials(username, password, grantType, clientID, clientSecret).Build()
+
+	if err := sdk.Authenticate(); err != nil {
+		log.Println(err)	
+	}
+
+	message := qontak.DirectWhatsAppBroadcast{
+		ToName: "Diky Yodihamzah",
+		ToNumber: "6289684279559",
+		MessageTemplateID: messageTemplateID,
+		ChannelIntegrationID: channelIntegrationID,
+		Language: map[string]string{
+			"code": "id",
+		},
+		BodyParams: []qontak.KeyValueText{
+			{
+				Key: "1",
+				Value: "full_name",
+				ValueText: "Diky Yodihamzah",
+			},
+		},
+	}
+
+	if err := sdk.SendDirectWhatsAppBroadcast(message); err != nil {
+		log.Println(err)
+	}
+}
